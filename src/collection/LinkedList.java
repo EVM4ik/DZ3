753ea22bc454f3dbb0a5702997932ab20b4a291d
Евгений <melnikov7400@yandex.ru>
2022-08-16 20:44:40 +0300

    first

---
+++ src/collection/LinkedList.java
@@ -0,0 +1,12 @@
+package collection;
+
+public class LinkedList {
+
+
+    public static final String OBJ1 = "";
+    public static final String OBJ2 = "";
+    public static final String OBJ3 = "";
+    public static final String OBJ4 = "";
+    public static final String OBJ5 = "";
+
+}
